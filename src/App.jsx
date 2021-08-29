f0b2c2c09213af8d0e2d3e0a0de00ddc8455f88f
Thuy Le <phuongthuy9796@gmail.com>
2021-08-29 12:36:09 -0700

    Add Material UI 🎨

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,41 @@
-import React from "react";
-import Header from "/Users/lethuy/my-app/src/components/Header.jsx";
-import Note from "/Users/lethuy/my-app/src/components/Note.jsx";
-import Footer from "/Users/lethuy/my-app/src/components/Footer.jsx";
-import notes from "./notes";
+import React, { useState } from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Note from "./components/Note";
+import CreateArea from "./components/CreateArea";
 
 function App() {
+  const [notes, setNotes] = useState([]);
+
+  function addNote(newNote) {
+    setNotes((prevNotes) => {
+      return [...prevNotes, newNote];
+    });
+  }
+
+  function deleteNote(id) {
+    setNotes((prevNotes) => {
+      return prevNotes.filter((noteItem, index) => {
+        return index !== id;
+      });
+    });
+  }
+
   return (
     <div>
       <Header />
-      {notes.map((note) => (
-        <Note title={note.title} content={note.content} />
-      ))}
+      <CreateArea onAdd={addNote} />
+      {notes.map((noteItem, index) => {
+        return (
+          <Note
+            key={index}
+            id={index}
+            title={noteItem.title}
+            content={noteItem.content}
+            onDelete={deleteNote}
+          />
+        );
+      })}
       <Footer />
     </div>
   );
